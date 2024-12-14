87bbe961c60d5b59134c19e6d34d7bc61bc69de1
root <root@Darth-Vader.localdomain>
2024-12-14 17:07:16 +0530

    v2.0

--- list-branch.sh
+++ list-branch.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
+###############
+# Author: Snehasish Mallik
+# Date: 12-14-2024
+# Purpose: List all the branches in a repo from CLI
+###############
+
 API_URL="https://api.github.com"
 
+# You can export and use them here
 USERNAME=$username
 TOKEN=$token
 
-REPOW="snehasish-mallik"
-REPON="HCLInsurance"
+# Pass these values while running the command
+REPOW=$1
+REPON=$2
 
-function get-all-branch{
-local endpoint="repos/${REPOW}/${REPON}/branches"
-local url="${API_URL}/${endpoint}"
-branches=$(curl -s -u "${USERNAME}:${TOKEN}" "$url")
-if [[ -z "$branches" ]]; then
-echo "zero branch"
-else
-echo "$branches"
-fi
+# Function which first creates the URL and gets the data from GitHub API using your credentials
+function get_all_branch {
+    local endpoint="repos/${REPOW}/${REPON}/branches"
+    local url="${API_URL}/${endpoint}"
+    branches=$(curl -s -u "${USERNAME}:${TOKEN}" "$url")
+    
+    if [[ -z "$branches" ]]; then
+        echo "Zero branches found."
+    else
+        echo "Branches in repository ${REPON}:"
+        echo "$branches" | jq -r '.[].name'
+    fi
 }
 
-get-all-branch
+# Calling and printing all the branches in a specific repo
+echo "Fetching all branches in $REPON..."
+get_all_branch
+
